eae201c85150c0e47255267d60a3a4cd7eeeb2a2
Teodor Potancok <teodor.potancok@gmail.com>
2025-07-15 19:12:49 +0200

    Move cast handler to SpellMechanics.kt

--- src/main/kotlin/ImmersiveMagic.kt
+++ src/main/kotlin/ImmersiveMagic.kt
@@ -6,17 +6,13 @@ import dev.tancop.immersivemagic.spells.FireballSpellComponent
 import dev.tancop.immersivemagic.spells.SpellComponent
 import net.minecraft.core.Registry
 import net.minecraft.core.component.DataComponentType
-import net.minecraft.core.component.DataComponents
-import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.core.registries.Registries
 import net.minecraft.resources.ResourceKey
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.tags.BlockTags
 import net.minecraft.tags.TagKey
-import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.player.Player
-import net.minecraft.world.item.component.CustomData
 import net.minecraft.world.item.crafting.RecipeSerializer
 import net.minecraft.world.item.crafting.RecipeType
 import net.minecraft.world.level.block.Block
@@ -47,41 +43,6 @@ class ImmersiveMagic(modEventBus: IEventBus, modContainer: ModContainer) {
         DATA_COMPONENT_TYPES.register(modEventBus)
         SPELL_COMPONENTS.register(modEventBus)
 
-
-        fun <T : PlayerInteractEvent> handleSpellCast(
-            event: T,
-            castFunction: SpellComponent.(T) -> InteractionResult,
-            onSuccess: (T) -> Unit
-        ) {
-            val heldItem = event.entity.getItemInHand(event.hand)
-
-            val map = ServerComponentMap.fromStack(heldItem) ?: return
-
-            val key = map.allKeys
-                .firstOrNull { SPELL_COMPONENTS_REGISTRY.containsKey(ResourceLocation.parse(it)) } ?: return
-            val resourceLocation = ResourceLocation.parse(key)
-
-            val componentType = BuiltInRegistries.DATA_COMPONENT_TYPE.get(resourceLocation) ?: return
-            val component = map.get(componentType)
-
-            if (component is SpellComponent) {
-                if (component.charges > 0) {
-                    val result = component.castFunction(event)
-                    if (result == InteractionResult.SUCCESS) {
-                        val newComponent = component.withLowerCharge()
-
-                        @Suppress("UNCHECKED_CAST") // We already checked the component's type
-                        map.set(componentType as DataComponentType<SpellComponent>, newComponent)
-
-                        heldItem.set(DataComponents.CUSTOM_DATA, CustomData.of(map.encode()))
-                        heldItem.set(DataComponents.LORE, newComponent.getItemLore())
-
-                        onSuccess(event)
-                        return
-                    }
-                }
-            }
-        }
 
         fun onRightClickBlock(event: PlayerInteractEvent.RightClickBlock) {
             if (event.level.isClientSide) return
---
+++ src/main/kotlin/SpellMechanics.kt
@@ -0,0 +1,46 @@
+package dev.tancop.immersivemagic
+
+import dev.tancop.immersivemagic.spells.SpellComponent
+import net.minecraft.core.component.DataComponentType
+import net.minecraft.core.component.DataComponents
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.item.component.CustomData
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent
+
+fun <T : PlayerInteractEvent> handleSpellCast(
+    event: T,
+    castFunction: SpellComponent.(T) -> InteractionResult,
+    onSuccess: (T) -> Unit
+) {
+    val heldItem = event.entity.getItemInHand(event.hand)
+
+    val map = ServerComponentMap.fromStack(heldItem) ?: return
+
+    val key = map.allKeys
+        .firstOrNull { ImmersiveMagic.Companion.SPELL_COMPONENTS_REGISTRY.containsKey(ResourceLocation.parse(it)) }
+        ?: return
+    val resourceLocation = ResourceLocation.parse(key)
+
+    val componentType = BuiltInRegistries.DATA_COMPONENT_TYPE.get(resourceLocation) ?: return
+    val component = map.get(componentType)
+
+    if (component is SpellComponent) {
+        if (component.charges > 0) {
+            val result = component.castFunction(event)
+            if (result == InteractionResult.SUCCESS) {
+                val newComponent = component.withLowerCharge()
+
+                @Suppress("UNCHECKED_CAST") // We already checked the component's type
+                map.set(componentType as DataComponentType<SpellComponent>, newComponent)
+
+                heldItem.set(DataComponents.CUSTOM_DATA, CustomData.of(map.encode()))
+                heldItem.set(DataComponents.LORE, newComponent.getItemLore())
+
+                onSuccess(event)
+                return
+            }
+        }
+    }
+}
